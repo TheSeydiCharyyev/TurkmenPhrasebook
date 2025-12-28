840b3de6f2ba2202ac0eec8c1bb6457d7d8862de
Seydi Charyyev <seydi.charyev@gmail.com>
2025-12-28 10:16:52 +0500

    Create CustomSplashScreen.tsx

---
+++ src/components/CustomSplashScreen.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+import { View, Image, Animated, StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+interface CustomSplashScreenProps {
+  onFinish: () => void;
+}
+
+export default function CustomSplashScreen({ onFinish }: CustomSplashScreenProps) {
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    // Показываем splash 2 секунды, потом fade out
+    const timer = setTimeout(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start(() => {
+        onFinish();
+      });
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [fadeAnim, onFinish]);
+
+  return (
+    <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
+      <Image
+        source={require('../../assets/splash.png')}
+        style={styles.image}
+        resizeMode="cover"
+      />
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#5BA3D9',
+    zIndex: 9999,
+  },
+  image: {
+    width: width,
+    height: height,
+  },
+});
